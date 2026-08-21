94a3b50921efc14919ff990111b7ae6826990bb6
Abdou Bakhe Niane <telemultiservices@gmail.com>
2026-08-21 19:18:41 +0000

    chore: remove temporary database test script

--- test-db.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config({ path: ".env.local" });
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-prisma.dossier.findFirst({ orderBy: { createdAt: "desc" } }).then(d => {
-  console.log("Recto:", d?.rectoUrl);
-  console.log("Verso:", d?.versoUrl);
-}).catch(console.error).finally(() => prisma.$disconnect());
